d4d5b1ba11b21ef9990003d798cf3f979fef1255
Muhammad Umair Raza <umair.raza101@gmail.com>
2023-03-21 02:38:57 +0500

    remove extra lines

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,5 +3,3 @@
 //= link administrate/application.js
 //= link dashboardChart.js
 //= link secretField.js
-//= link administrate-field-jsonb/application.js
-//= link administrate-field-jsonb/application.css
